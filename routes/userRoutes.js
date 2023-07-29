1d7741ebc8d4e031c6353a9bc2a109fdbdec4aa0
Arjun <arjun.20212039@mnnit.ac.in>
2023-07-29 21:43:17 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  loginController,
+  registerController,
+  authController,
+} = require("../controllers/userCtrl");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+//router object
+const router = express.Router();
+
+//routes
+//LOGIN || POST
+router.post("/login", loginController);
+
+//Register || POST
+
+router.post("/register", registerController);
+router.post("/getUserData", authMiddleware, authController);
+module.exports = router;
